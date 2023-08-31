5ad8c50cd4bb53d969b53ad52be00c4b166e6003
Nathan Chevalier <cerberusgfx@gmail.com>
2023-08-31 11:20:37 -0500

    Feature complete, bugs smashed, Chuckle Checklist done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./index.css";
-import { App } from "./App";
 import { createRoot } from "react-dom/client";
+import { App } from "./App";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
